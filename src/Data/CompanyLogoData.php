29f9f5086baf86e9cd2b43e8fbb5de88b4115a2a
Kim Larsson <kim@humbleguys.se>
2025-05-01 20:22:25 +0200

    wip

---
+++ src/Data/CompanyLogoData.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace SparcGroup\WebsiteCrmIntegration\Data;
+
+use Illuminate\Support\Collection;
+
+class CompanyLogoData {
+    public function __construct(
+        public int $id,
+        public string $name,
+        public string $logo
+    ) {}
+
+    public static function fromArray(array $data): self
+    {
+        return new self(
+            id: $data['id'],
+            name: $data['name'],
+            logo: $data['logo'],
+        );
+    }
+
+    public static function collect(array $items): Collection
+    {
+        return collect($items)->map(fn (array $item) => self::fromArray($item));
+    }
+}
